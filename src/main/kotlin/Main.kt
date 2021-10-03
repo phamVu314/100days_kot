c954dcee9f1cc45737f7cb2d22ede39bd11a2c15
pham vu <makeyougreatagain.vu@gmail.com>
2021-10-03 14:48:09 +0700

    day 0

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+class Main {
+
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val a: Int? = 1000
+            val b: Int? = 1000
+
+            print(a === b)
+        }
+
+    }
+
+}
